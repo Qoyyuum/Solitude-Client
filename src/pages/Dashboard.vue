01226f594aa7a8fcff60af85e7d3866f55def2e3
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2021-02-08 10:11:31 +0800

    Login look and feel 1st Draft

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding>
+      <q-calendar
+      v-model="selectedDate"
+      view="month"
+      locale="en-us"
+      :day-height="100"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data () {
+    return {
+      selectedDate : '2021/02/08'
+    }
+  }
+}
+</script>
