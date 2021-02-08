a425fc0a102e4fe36cebed4974ddfb81dc88d602
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2021-02-08 16:35:38 +0800

    :construction: Calendar looks done. Form pending.

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,20 +1,291 @@
 <template>
   <q-page padding>
-      <q-calendar
+    <div class="row justify-center items-center">
+      <q-btn dense flat label="Prev" @click="calendarPrev" />
+      <q-separator vertical />
+      <q-btn dense flat label="Next" @click="calendarNext" />
+    </div>
+    <q-calendar
+      ref="calendar"
       v-model="selectedDate"
       view="month"
       locale="en-us"
       :day-height="100"
-    />
+      v-touch-swipe.mouse.left.right="handleSwipe"
+      animated
+      transition-prev="slide-right"
+      transition-next="slide-left"
+    >
+      <template #day="{ timestamp }">
+        <template v-for="(event, index) in getEvents(timestamp.date)">
+          <q-badge
+            :key="index"
+            style="width: 100%; cursor: pointer; height: 16px; max-height: 16px"
+            :class="badgeClasses(event, 'day')"
+            :style="badgeStyles(event, 'day')"
+          >
+            <q-icon v-if="event.icon" :name="event.icon" class="q-mr-xs"></q-icon><span class="ellipsis">{{ event.title }}</span>
+          </q-badge>
+        </template>
+      </template>
+    </q-calendar>
+
+    <!--TODO: Make a working logout button and maybe place it in the Essentials Link-->
+    <q-btn
+          label="Logout"
+          type="submit"
+          color="negative"
+          to="/"
+        />
   </q-page>
 </template>
 
 <script>
+import {ui, addToDate, parseTimestamp, parseDate, isBetweenDates} from '@quasar/quasar-ui-qcalendar'
+
+const CURRENT_DAY = new Date()
+
+function getCurrentDay (day) {
+  const newDay = new Date(CURRENT_DAY)
+  newDay.setDate(day)
+  const tm = parseDate(newDay)
+  return tm.date
+}
+
+const reRGBA = /^\s*rgb(a)?\s*\((\s*(\d+)\s*,\s*?){2}(\d+)\s*,?\s*([01]?\.?\d*?)?\s*\)\s*$/
+
+function textToRgb (color) {
+  if (typeof color !== 'string') {
+    throw new TypeError('Expected a string')
+  }
+
+  const m = reRGBA.exec(color)
+  if (m) {
+    const rgb = {
+      r: Math.min(255, parseInt(m[2], 10)),
+      g: Math.min(255, parseInt(m[3], 10)),
+      b: Math.min(255, parseInt(m[4], 10))
+    }
+    if (m[1]) {
+      rgb.a = Math.min(1, parseFloat(m[5]))
+    }
+    return rgb
+  }
+  return hexToRgb(color)
+}
+
+function hexToRgb (hex) {
+  if (typeof hex !== 'string') {
+    throw new TypeError('Expected a string')
+  }
+
+  hex = hex.replace(/^#/, '')
+
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
+  }
+  else if (hex.length === 4) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2] + hex[3] + hex[3]
+  }
+
+  const num = parseInt(hex, 16)
+
+  return hex.length > 6
+    ? { r: num >> 24 & 255, g: num >> 16 & 255, b: num >> 8 & 255, a: Math.round((num & 255) / 2.55) }
+    : { r: num >> 16, g: num >> 8 & 255, b: num & 255 }
+}
+
+function luminosity (color) {
+  if (typeof color !== 'string' && (!color || color.r === undefined)) {
+    throw new TypeError('Expected a string or a {r, g, b} object as color')
+  }
+
+  const
+    rgb = typeof color === 'string' ? textToRgb(color) : color,
+    r = rgb.r / 255,
+    g = rgb.g / 255,
+    b = rgb.b / 255,
+    R = r <= 0.03928 ? r / 12.92 : Math.pow((r + 0.055) / 1.055, 2.4),
+    G = g <= 0.03928 ? g / 12.92 : Math.pow((g + 0.055) / 1.055, 2.4),
+    B = b <= 0.03928 ? b / 12.92 : Math.pow((b + 0.055) / 1.055, 2.4)
+  return 0.2126 * R + 0.7152 * G + 0.0722 * B
+}
+
 export default {
-  name: 'Dashboard',
   data () {
     return {
-      selectedDate : '2021/02/08'
+      selectedDate: '2021/02/08',
+      events: [
+        {
+          title: '1st of the Month',
+          details: 'Everything is funny as long as it is happening to someone else',
+          date: getCurrentDay(1),
+          bgcolor: 'orange'
+        },
+        {
+          title: 'Sisters Birthday',
+          details: 'Buy a nice present',
+          date: getCurrentDay(4),
+          bgcolor: 'green',
+          icon: 'fas fa-birthday-cake'
+        },
+        {
+          title: 'Meeting',
+          details: 'Time to pitch my idea to the company',
+          date: getCurrentDay(8),
+          time: '10:00',
+          duration: 120,
+          bgcolor: 'red',
+          icon: 'fas fa-handshake'
+        },
+        {
+          title: 'Lunch',
+          details: 'Company is paying!',
+          date: getCurrentDay(8),
+          time: '11:30',
+          duration: 90,
+          bgcolor: 'teal',
+          icon: 'fas fa-hamburger'
+        },
+        {
+          title: 'Visit mom',
+          details: 'Always a nice chat with mom',
+          date: getCurrentDay(20),
+          time: '17:00',
+          duration: 90,
+          bgcolor: 'blue-grey',
+          icon: 'fas fa-car'
+        },
+        {
+          title: 'Conference',
+          details: 'Teaching Javascript 101',
+          date: getCurrentDay(15),
+          time: '08:00',
+          duration: 540,
+          bgcolor: 'blue',
+          icon: 'fas fa-chalkboard-teacher'
+        },
+        {
+          title: 'Girlfriend',
+          details: 'Meet GF for dinner at Swanky Restaurant',
+          date: getCurrentDay(15),
+          time: '19:00',
+          duration: 180,
+          bgcolor: 'teal',
+          icon: 'fas fa-utensils'
+        },
+        {
+          title: 'Rowing',
+          details: 'Time for some weekend R&R',
+          date: getCurrentDay(16),
+          bgcolor: 'purple',
+          icon: 'rowing',
+          days: 2
+        },
+        {
+          title: 'Vacation',
+          details: 'Trails and hikes, going camping! Don\'t forget to bring bear spray!',
+          date: getCurrentDay(22),
+          bgcolor: 'purple',
+          icon: 'fas fa-plane',
+          days: 7
+        }
+      ]
+    }
+  },
+  methods: {
+    calendarNext () {
+      this.$refs.calendar.next()
+    },
+    calendarPrev () {
+      this.$refs.calendar.prev()
+    },
+    handleSwipe ({ evt, ...info }) {
+      if (info.duration >= 30) {
+        if (info.direction === 'right') {
+          this.calendarPrev()
+        }
+        else if (info.direction === 'left') {
+          this.calendarNext()
+        }
+      }
+      evt.cancelable !== false && evt.preventDefault()
+      evt.stopPropagation()
+    },
+
+    isCssColor (color) {
+      return !!color && !!color.match(/^(#|(rgb|hsl)a?\()/)
+    },
+
+    badgeClasses (event, type) {
+      const cssColor = this.isCssColor(event.bgcolor)
+      const isHeader = type === 'header'
+      return {
+        [`text-white bg-${event.bgcolor}`]: !cssColor,
+        'full-width': !isHeader && (!event.side || event.side === 'full'),
+        'left-side': !isHeader && event.side === 'left',
+        'right-side': !isHeader && event.side === 'right'
+      }
+    },
+
+    badgeStyles (event, type, timeStartPos, timeDurationHeight) {
+      const s = {}
+      if (this.isCssColor(event.bgcolor)) {
+        s['background-color'] = event.bgcolor
+        s.color = luminosity(event.bgcolor) > 0.5 ? 'black' : 'white'
+      }
+      if (timeStartPos) {
+        s.top = timeStartPos(event.time) + 'px'
+      }
+      if (timeDurationHeight) {
+        s.height = timeDurationHeight(event.duration) + 'px'
+      }
+      s['align-items'] = 'flex-start'
+      return s
+    },
+
+    getEvents (dt) {
+      const currentDate = parseTimestamp(dt)
+      const events = []
+      for (let i = 0; i < this.events.length; ++i) {
+        let added = false
+        if (this.events[i].date === dt) {
+          if (this.events[i].time) {
+            if (events.length > 0) {
+              // check for overlapping times
+              const startTime = parseTimestamp(this.events[i].date + ' ' + this.events[i].time)
+              const endTime = addToDate(startTime, { minute: this.events[i].duration })
+              for (let j = 0; j < events.length; ++j) {
+                if (events[j].time) {
+                  const startTime2 = parseTimestamp(events[j].date + ' ' + events[j].time)
+                  const endTime2 = addToDate(startTime2, { minute: events[j].duration })
+                  if (isBetweenDates(startTime, startTime2, endTime2) || isBetweenDates(endTime, startTime2, endTime2)) {
+                    events[j].side = 'left'
+                    this.events[i].side = 'right'
+                    events.push(this.events[i])
+                    added = true
+                    break
+                  }
+                }
+              }
+            }
+          }
+          if (!added) {
+            this.events[i].side = undefined
+            events.push(this.events[i])
+          }
+        }
+        else if (this.events[i].days) {
+          // check for overlapping dates
+          const startDate = parseTimestamp(this.events[i].date)
+          const endDate = addToDate(startDate, { day: this.events[i].days })
+          if (isBetweenDates(currentDate, startDate, endDate)) {
+            events.push(this.events[i])
+            added = true
+          }
+        }
+      }
+      return events
     }
   }
 }
