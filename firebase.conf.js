0860373a2e9fa03e70cf5a5642b19bb8b46e1f7a
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2021-02-16 16:05:17 +0800

    Update and rename samplefirebase.conf.js to firebase.conf.js
    According to the Internet, its safe to put this in the repo, so others can test on their local machine as well.

---
+++ firebase.conf.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCOAc6Bd0HXjDyaBmB4Lc6HRAIuB0I-aU8",
+    authDomain: "solitude-c2da9.firebaseapp.com",
+    projectId: "solitude-c2da9",
+    storageBucket: "solitude-c2da9.appspot.com",
+    messagingSenderId: "96010132994",
+    appId: "1:96010132994:web:6c50952f34468133ba911b",
+    measurementId: "G-F17RB696M6"
+};
