b499959f63b02e1cac85b2f2ff619d092da4f67b
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2021-03-01 15:24:15 +0800

    :construction: WIP Calendar Integration

---
+++ src/boot/calendar.js
@@ -0,0 +1,72 @@
+const client_secret = process.env.CALENDAR_SECRET
+const client_id     = process.env.CALENDAR_CLIENT
+const redirect_uris = ["https://accounts.google.com/o/oauth2/auth", "https://oauth2.googleapis.com/token"]
+const provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
+const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+const {google} = require('googleapis');
+
+
+function authorize(client_id, client_secret, redirect_uris, callback) {
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+function listEvents(auth) {
+  const calendar = google.calendar({version: 'v3', auth});
+  calendar.events.list({
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const events = res.data.items;
+    if (events.length) {
+      console.log('Upcoming 10 events:');
+      events.map((event, i) => {
+        const start = event.start.dateTime || event.start.date;
+        console.log(`${start} - ${event.summary}`);
+      });
+    } else {
+      console.log('No upcoming events found.');
+    }
+  });
+}
+
+authorize(client_id, client_secret, redirect_uris)
+
+// TODO: Alternatively look at the VueJS sample and twist from there
+// https://jsfiddle.net/andrisv/nqwdymee/
--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,14 +1,6 @@
 import Firebase from 'firebase/app'
 import "firebase/auth"
-// import 'firebase/firestore' // eslint-disable-line
 import firebaseConfig from '../../firebase.conf'
-
-// export default ({ Vue }) => {
-//   // Initialize Firebase from settings
-//   Firebase.initializeApp(firebaseConfig)
-
-//   Vue.prototype.$firebase = Firebase
-// }
 
 let firebaseApp = Firebase.initializeApp(firebaseConfig)
 let firebaseAuth = firebaseApp.auth()
